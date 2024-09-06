44adf22dc7d0cf3fe01e4d89e80da6e1bc691be7
Ktento <kento07210525mo@outlook.jp>
2024-09-06 09:36:06 +0900

    テーブルの変更

--- db/migrate/20240904022943_create_job_hunting_infos.rb
+++ db/migrate/20240904022943_create_job_hunting_infos.rb
@@ -2,9 +2,9 @@ class CreateJobHuntingInfos < ActiveRecord::Migration[7.2]
   def change
     create_table :job_hunting_infos do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :self_pr
-      t.string :motivation
-      t.string :extracurricular_activities
+      t.string :self_pr, limit: 400
+      t.string :motivation, limit: 400
+      t.string :extracurricular_activities, limit: 400
 
       t.timestamps
     end
